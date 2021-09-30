2712c6cca3708355247306e5e2c40cb6abade890
Sh4ke96 <Sh4ke96@gmail.com>
2021-09-30 23:11:43 +0200

    add bg-img for login page, basic RWD for this component

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -12,75 +12,92 @@ import {
 function Login() {
   return (
     <Container>
-      <div className="heading">
-        <img className="heading-logo" src="images/logo.svg" alt="vuexy-logo" />
-        <span className="heading-subtitle">Vuexy</span>
-      </div>
-      <h1 className="heading-title">Welcome to Vuexy! 👋</h1>
-      <span className="heading-text">
-        Please sign-in to your account and start the adventure.
-      </span>
-      <div className="heading-modal">
-        <span className="heading-modal-title">
-          Admin: admin@demo.com | admin
-        </span>
-        <span className="heading-modal-title">
-          Client: client@demo.com | client
-        </span>
-      </div>
-      <div>
-        <div className="heading-label-container">
-          <label className="heading-label" htmlFor="email">
-            Email
-            <input
-              className="heading-input"
-              name="email"
-              placeholder="admin@demo.com"
-            ></input>
-          </label>
+      <header>
+        <div className="heading">
+          <img
+            className="heading-logo"
+            src="images/logo.svg"
+            alt="vuexy-logo"
+          />
+          <span className="heading-name">Vuexy</span>
         </div>
-        <div className="heading-label-container">
-          <label className="heading-label" htmlFor="password">
-            Password
-            <input
-              className="heading-input"
-              type="password"
-              name="password"
-              defaultValue="admin"
-            ></input>
-          </label>
+      </header>
+      <main>
+        <div className="left-container">
+          <img
+            className="login-bg"
+            src="images/login-bg.svg"
+            alt="5 people sitting"
+          />
         </div>
-        <div className="heading-remember">
-          <label className="heading-remember" htmlFor="remeber me">
-            Remember Me
-            <input
-              className="heading-remember-checkbox"
-              type="checkbox"
-              name="remember me"
-            ></input>
-          </label>
-        </div>
-        <Button className="heading-button">Sign In</Button>
-        <div className="heading-socials">
-          <span className="heading-socials-title">
-            New on our platform?{" "}
-            <Link to="/register" className="heading-socials-link">
-              Create an account
-            </Link>
+        <div className="right-container">
+          <h1 className="heading-title">Welcome to Vuexy! 👋</h1>
+          <span className="heading-text">
+            Please sign-in to your account and start the adventure.
           </span>
-          <div className="heading-socials-divider">
-            <div className="heading-socials-divider-element"></div>
-            <div className="heading-socials-divider-text">or</div>
-            <div className="heading-socials-divider-element"></div>
+          <div className="heading-modal">
+            <span className="heading-modal-title">
+              Admin: admin@demo.com | admin
+            </span>
+            <span className="heading-modal-title">
+              Client: client@demo.com | client
+            </span>
+          </div>
+          <div>
+            <div className="heading-label-container">
+              <label className="heading-label" htmlFor="email">
+                Email
+                <input
+                  className="heading-input"
+                  name="email"
+                  placeholder="admin@demo.com"
+                ></input>
+              </label>
+            </div>
+            <div className="heading-label-container">
+              <label className="heading-label" htmlFor="password">
+                Password
+                <input
+                  className="heading-input"
+                  type="password"
+                  name="password"
+                  defaultValue="admin"
+                ></input>
+              </label>
+            </div>
+            <div className="heading-remember">
+              <label className="heading-remember" htmlFor="remeber me">
+                Remember Me
+                <input
+                  className="heading-remember-checkbox"
+                  type="checkbox"
+                  name="remember me"
+                ></input>
+              </label>
+            </div>
+            <Button className="heading-button">Sign In</Button>
+            <div className="heading-socials">
+              <span className="heading-socials-title">
+                New on our platform?{" "}
+                <Link to="/register" className="heading-socials-link">
+                  Create an account
+                </Link>
+              </span>
+              <div className="heading-socials-divider">
+                <div className="heading-socials-divider-element"></div>
+                <div className="heading-socials-divider-text">or</div>
+                <div className="heading-socials-divider-element"></div>
+              </div>
+            </div>
+            <div className="heading-socials-container">
+              <FaFacebookF className="heading-socials-icon" />
+              <FaTwitter className="heading-socials-icon" />
+              <FaEnvelope className="heading-socials-icon" />
+              <FaGithubAlt className="heading-socials-icon" />
+            </div>
           </div>
         </div>
-        <div className="heading-socials-container">
-          <FaFacebookF className="heading-socials-icon" />
-          <FaTwitter className="heading-socials-icon" />
-          <FaEnvelope className="heading-socials-icon" />
-          <FaGithubAlt className="heading-socials-icon" />
-        </div>
-      </div>
+      </main>
     </Container>
   );
 }
--- src/components/login/login.styled.js
+++ src/components/login/login.styled.js
@@ -2,142 +2,170 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   padding: 1rem 2rem;
-  .heading {
-    display: flex;
-    align-items: center;
-    .heading-logo {
-      max-width: 100%;
-      width: 32px;
-      height: auto;
-    }
-    .heading-subtitle {
-      font-size: 1.4rem;
-      font-weight: bold;
-      margin-left: 0.5rem;
-      color: #7367f0;
-    }
-  }
-  .heading-title {
-    font-size: 1.3rem;
-    font-weight: normal;
-    line-height: 1.2;
-    margin: 1rem 0;
-    color: #5e5873;
-  }
-  .heading-text {
-    color: #6e6b7b;
-    line-height: 1.5;
-    font-size: 0.9rem;
-    margin-bottom: 0.5rem;
-  }
-  .heading-modal {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background: rgba(115, 103, 240, 0.12);
-    border-radius: 10px;
-    padding: 0.7rem 1rem;
-    margin: 0.8rem 0;
-    color: #7367f0;
-    .heading-modal-title {
-      font-size: 0.9rem;
-      margin: 0.2rem 0;
+  header {
+    max-height: 30px;
+    .heading {
+      display: flex;
+      align-items: center;
+      .heading-logo {
+        max-width: 100%;
+        width: 32px;
+        height: auto;
+      }
+      .heading-name {
+        font-size: 1.4rem;
+        font-weight: bold;
+        margin-left: 0.5rem;
+        color: #7367f0;
+      }
     }
   }
-  .heading-label-container {
+  main {
     display: flex;
-    flex-direction: column;
-    margin: 1rem 0;
-    .heading-label {
+    height: calc(100vh - 60px);
+    .left-container {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex: 0 0 66.66667%;
+      max-width: 66.66667%;
+      margin-right: 1rem;
+      .login-bg {
+        max-width: 100%;
+        height: auto;
+      }
+    }
+    .right-container {
       display: flex;
       flex-direction: column;
-      font-size: 0.8rem;
-      color: #5e5873;
-    }
-    .heading-input {
-      padding: 0.4rem 1rem;
-      font-size: 0.9rem;
-      margin-top: 0.3rem;
-      background-color: #fff;
-      border: 1px solid #d8d6de;
-      color: #6e6b7b;
-      outline: none;
-    }
-    .heading-input:focus {
-      outline: 1px solid #7367f0;
-    }
-    .heading-input::placeholder {
-      font-size: 0.8rem;
-    }
-  }
-  .heading-remember {
-    display: flex;
-    align-items: center;
-    flex-direction: row-reverse;
-    justify-content: flex-end;
-    margin-bottom: 0.5rem;
-    .heading-remember {
-      font-size: 0.9rem;
-      color: #6e6b7b;
-      .heading-remember-checkbox {
-        height: 16px;
-        width: 16px;
-        margin-right: 0.4rem;
-        accent-color: #7367f0;
-        cursor: pointer;
+      justify-content: center;
+      align-items: center;
+      flex: 0 0 33.33333%;
+      max-width: 33%;
+      padding: 0 4rem;
+      .heading-title {
+        font-size: 1.3rem;
+        font-weight: normal;
+        line-height: 1.2;
+        margin: 1rem 0;
+        color: #5e5873;
       }
-    }
-  }
-  .heading-button {
-    margin: 1rem 0;
-    width: 100%;
-  }
-  .heading-socials {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin: 1rem 0;
-    .heading-socials-title {
-      color: #6e6b7b;
-      font-size: 0.9rem;
-      .heading-socials-link {
+      .heading-text {
+        color: #6e6b7b;
+        line-height: 1.5;
+        font-size: 0.9rem;
+        margin-bottom: 0.5rem;
+      }
+      .heading-modal {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background: rgba(115, 103, 240, 0.12);
+        border-radius: 10px;
+        padding: 0.7rem 1rem;
+        margin: 0.8rem 0;
         color: #7367f0;
-        text-decoration: none;
+        .heading-modal-title {
+          font-size: 0.9rem;
+          margin: 0.2rem 0;
+        }
       }
-    }
-  }
-  .heading-socials-divider {
-    display: flex;
-    width: 100%;
-    align-items: center;
-    margin: 1.5rem 0 0.5rem 0;
-    .heading-socials-divider-text {
-      color: #6e6b7b;
-      font-size: 0.9rem;
-    }
-    .heading-socials-divider-element {
-      height: 1px;
-      width: 100%;
-      background-color: #ebe9f1;
-      margin: 0 0.5rem;
-    }
-  }
-  .heading-socials-container {
-    display: flex;
-    justify-content: center;
-    .heading-socials-icon {
-      font-size: 1.5rem;
-      width: 30px;
-      height: 30px;
-      padding: 0.4rem;
-      border-radius: 10px;
-      margin: 0 0.5rem;
-      background-color: #7367f0;
-      color: #fff;
-    }
-    .heading-socials-icon:hover {
-      cursor: pointer;
-      box-shadow: 0 8px 25px -8px #7367f0;
+      .heading-label-container {
+        display: flex;
+        flex-direction: column;
+        margin: 1rem 0;
+        .heading-label {
+          display: flex;
+          flex-direction: column;
+          font-size: 0.8rem;
+          color: #5e5873;
+        }
+        .heading-input {
+          padding: 0.4rem 1rem;
+          font-size: 0.9rem;
+          margin-top: 0.3rem;
+          background-color: #fff;
+          border: 1px solid #d8d6de;
+          color: #6e6b7b;
+          outline: none;
+        }
+        .heading-input:focus {
+          outline: 1px solid #7367f0;
+        }
+        .heading-input::placeholder {
+          font-size: 0.8rem;
+        }
+      }
+      .heading-remember {
+        display: flex;
+        align-items: center;
+        flex-direction: row-reverse;
+        justify-content: flex-end;
+        margin-bottom: 0.5rem;
+        .heading-remember {
+          font-size: 0.9rem;
+          color: #6e6b7b;
+          .heading-remember-checkbox {
+            height: 16px;
+            width: 16px;
+            margin-right: 0.4rem;
+            accent-color: #7367f0;
+            cursor: pointer;
+          }
+        }
+      }
+      .heading-button {
+        margin: 1rem 0;
+        width: 100%;
+      }
+      .heading-socials {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin: 1rem 0;
+        .heading-socials-title {
+          color: #6e6b7b;
+          font-size: 0.9rem;
+          .heading-socials-link {
+            color: #7367f0;
+            text-decoration: none;
+          }
+        }
+      }
+      .heading-socials-divider {
+        display: flex;
+        width: 100%;
+        align-items: center;
+        margin: 1.5rem 0 0.5rem 0;
+        .heading-socials-divider-text {
+          color: #6e6b7b;
+          font-size: 0.9rem;
+        }
+        .heading-socials-divider-element {
+          height: 1px;
+          width: 100%;
+          background-color: #ebe9f1;
+          margin: 0 0.5rem;
+        }
+      }
+      .heading-socials-container {
+        display: flex;
+        justify-content: center;
+        .heading-socials-icon {
+          font-size: 1.5rem;
+          width: 30px;
+          height: 30px;
+          padding: 0.4rem;
+          border-radius: 10px;
+          margin: 0 0.5rem;
+          background-color: #7367f0;
+          color: #fff;
+        }
+        .heading-socials-icon:hover {
+          cursor: pointer;
+          box-shadow: 0 8px 25px -8px #7367f0;
+        }
+      }
     }
   }
 `;
